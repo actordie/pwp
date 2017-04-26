ef8f684d455411c1d2cb74c9eed018d167ab3186
S JACKSON <jackson.sabastian@gmail.com>
2017-04-26 15:36:45 -0600

    hello darkness my old friend ;-)

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE HTML>
+<html>
+	<meta charset="utf-8">
+	<!-- Purpose - Who is this for?:
+	 Audience -  Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of? Could it be for your professional peers and other developers, potential employers, customers, or clients, etc.?
+	 Goal: This is should be a quantifiable goal if possible. For example, a goal for your PWP could be:
+To acquire full or part-time employment or an internship.
+To increase sales and/or conversion rates for an existing business.
+To attract freelance clients.
+To serve as a gallery to showcase and promote your work.
+To increase the reach of a new or established social media presence (as measured via analytics tools).-->
+	<head>
+<h1>PWP Milestone One</h1>
+	</head>
+	<body>
+		<h2>Purpose, Audience Goal</h2>
+		<h3>Purpose</h3>
+		<p>The purpose of this webssite is to thank those who have given me the opprotunity to complete the Bootcamp </p>
+		<h3>Audience</h3>
+		<p>The Audience should the New Mexico Workforce Connection Center, CNM, and the Kellog Foundation</p>
+		<h3>Goal</h3>
+		<p>The goal of this project is to show off what I have accomplished in this program</p>
+		<h2>Persona</h2>
+		<b><p>Name:</b>La June Montgomery Tabron</p>
+		<p><b>Age</b>:50s
+		<p><b>Profession</b>:CEO of the The W. K. Kellogg Foundation</p>
+		<p><b>Tech</b>:iPad at the office, probably with an Android because it's much better than iPhone and she is very smart.</p>
+		<p><b>Attitudes:</b>Short on time, overworked, and has a full social schedule in addition. Doesn't use social media often so anything brought to her attention is considerable if it is engaging</p>
+		<p><b>Frustrations & Needs</b>Frustrated with the inequality, racism, and classism throughout hte states she would need to know how well her program is doing to help those in those shoes. </p>
+
+		<h2>Use Case</h2>
+		<p>La June Montgomery Tabron the CEO of the Kellogg Foundation. I was told never to ask a CEO of a billion dollar corporation her age but if I had to guess she is in her mid-50s, using an iPad to view my site after finding out how well her grant program had worked.</p>
+		<!-- would love to make it interactive to have it worth more of her attention -->
+		<p>It's 9:25 am on Monday in Michigan and she is having her daily breifing meeting with her immediate staff. La June probably wouldn't have much time so it would be a quick view. This woman is probably pressed for time so it would definitley be worth her time or she won't be too interested in knowing more about the message before the website.</p>
+
+	</body>
+</html>
+
