fc826358af97ad9f2d9a69c73d00b4c682da6de0
S JACKSON <jackson.sabastian@gmail.com>
2017-05-14 11:21:47 -0600

    revisions

---
+++ public_html/documentation/milesotne-2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+	<meta charset="utf-8">
+	<head>
+
+
+	<body>
+		<h1>Splash</h1>
+		<p>Splashpage with a background image</p>
+		<h1>Nav</h1>
+		<p>Transparent Nav Bar that becomes opaque and settles at top of screen as user scrolls</p>
+		<ul>
+			<li>About Me</li>
+			<li>My Work</li>
+			<li>Thank you page</li>
+			<li>Gallery</li>
+			<li></li>
+		</ul>
+		<h1>Welcome</h1>
+		<p>Welcome messaging</p>
+		<img src="images/ambientcreativit.png">
+		<img src="images/ambientcreativit-1.png">
+		<img src="images/ambientcreativit-2.png">
+
+	</body>
+
+	</head>
+</html>
