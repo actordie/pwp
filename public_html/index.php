29a4004f2ad0d28db839854a786a3cbb738461df
S JACKSON <jackson.sabastian@gmail.com>
2017-05-25 15:14:39 -0600

    added last image

--- public_html/index.php
+++ public_html/index.php
@@ -6,10 +6,16 @@
 	</head>
 	<body>
 		<section>
-			<img src=""
-		</section>>
+			<img src="images/6254586.jpg">
+		</section>
 		<section>
-			<img src="images"
+			<img src="images/14487422_264631550604799_484667706230964224_n.jpg">
+		</section>
+		<section>
+			<img src="images/linkin.jpg">
+		</section>
+		<section>
+			<img src="images/GOPR0082.MP4.00_00_45_13.Still001.jpg"
 		</section>
 	</body>
 </html>
