cd923381a64506fd8ea5b335b04006a4462d1a15
S JACKSON <jackson.sabastian@gmail.com>
2017-05-29 15:30:34 -0600

    added new image in place of css and add CSS

--- public_html/index.php
+++ public_html/index.php
@@ -3,10 +3,11 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Skate Smart, Live Fast</title>
+		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
 		<section>
-			<img src="images/6254586.jpg">
+			<img src="images/puma.jpg" class="background-image">
 		</section>
 		<section>
 			<img src="images/14487422_264631550604799_484667706230964224_n.jpg">
