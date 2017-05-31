5a04502ac09af971b4f845b6b1304c8f9bf1dfed
S JACKSON <jackson.sabastian@gmail.com>
2017-05-31 17:16:34 -0600

    added links to navbar - need to center them soon

--- public_html/index.php
+++ public_html/index.php
@@ -3,15 +3,29 @@
 	<head>
 		<title>Skate Smart, Live Fast</title>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name=“viewport” content=“width=device-width, initial-scale=1">
+
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
@@ -19,7 +33,7 @@
 	</head>
 	<body>
 		<nav class="navbar navbar-fixed-top navbar-transparent" role="navigation">
-			<div class="container-fluid">
+			<div class="container-fluid" id="nav_center">
 				<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
@@ -29,42 +43,28 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#page-top">ACT OR DIE</a>
+					<a class="navbar-brand" href="#about-me">ACT OR DIE</a>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav">
-						<li class="active"><a href="#"> About Me <span class="sr-only">(current)</span></a></li>
-						<li><a href="#">Acknowledgements</a></li>
-						<li><a href="#">Gallery</a></li>
-						<li><a href="#">Contact</a></li>
-						</li>
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Link</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-								aria-expanded="false">Dropdown <span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Action</a></li>
-								<li><a href="#">Another action</a></li>
-								<li><a href="#">Something else here</a></li>
-								<li role="separator" class="divider"></li>
-								<li><a href="#">Separated link</a></li>
-							</ul>
+						<li class="active"><a href="#about-me"> About Me <span class="sr-only">(current)</span></a></li>
+						<li><a href="#acknowledgements">Acknowledgements</a></li>
+						<li><a href="#gallery">Gallery</a></li>
+						<li><a href="#contact">Contact</a></li>
 						</li>
 					</ul>
 				</div><!-- /.navbar-collapse -->
 			</div><!-- /.container-fluid -->
 		</nav>
 
-		<div class="background-image-1">
+		<div class="background-image-1" id="about-me">
 		</div>
-		<div class="background-image-2">
+		<div class="background-image-2" id="acknowledgements">
 		</div>
-		<div class="background-image-3">
+		<div class="background-image-3" id="gallery">
 		</div>
-		<div class="background-image-4">
+		<div class="background-image-4" id="contact">
 		</div>
 	</body>
 </html>
