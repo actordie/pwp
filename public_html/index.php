cb15b295c258c182e0c6ae35b217f03d8ef8bd53
S JACKSON <jackson.sabastian@gmail.com>
2017-06-06 10:03:02 -0600

    fancybox is not playing nice on mobile - gallery seems to be showing up but no actions are coming from it
    culprit = javascript?

--- public_html/index.php
+++ public_html/index.php
@@ -7,29 +7,28 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name=“viewport” content=“width=device-width, initial-scale=1">
 
+
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!-- jQuery v3.0 -->
+
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-
-		<!-- jQuery v3.0 -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
-
 	</head>
 	<body>
 
@@ -50,7 +49,7 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav">
-							<li class="active"><a href="#about-me"> About Me <span class="sr-only">(current)</span></a></li>
+							<li><a href="#about-me"> About Me <span class="sr-only">(current)</span></a></li>
 							<li><a href="#acknowledgements">Acknowledgements</a></li>
 							<li><a href="#gallery">Gallery</a></li>
 							<li><a href="#contact">Contact</a></li>
@@ -82,15 +81,71 @@
 
 		<div id="gallery">
 			<div class="container-fluid">
-				images here
-			</div>
-			<!--<a data-fancybox="gallery" href="images/regina-revenge.jpg">
-				<img src="images/regina-revenge.jpg">
-			</a>
+				<a data-fancybox="gallery" href="images/regina-revenge.jpg">
+					<img src="images/regina-revenge.jpg">
+				</a>
 
-			<a data-fancybox="gallery" href="images/pov-resize.jpg">
-				<img src="images/pov-resize.jpg">
-			</a> -->
+				<a data-fancybox="gallery" href="images/pov-resize.jpg">
+					<img src="images/pov-resize.jpg">
+				</a>
+			</div>
+		</div>
+
+
+		<!--Begin Contact Form-->
+		<div>
+			<form id="contact-form" action="php/mailer.php" method="post">
+				<div class="form-group">
+					<label for="name">Name <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="email">Email <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</div>
+						<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="subject">Subject</label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-pencil" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="message">Message <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-comment" aria-hidden="true"></i>
+						</div>
+						<textarea class="form-control" rows="5" id="message" name="message"
+									 placeholder="Message (2000 characters max)"></textarea>
+					</div>
+				</div>
+
+				<!-- reCAPTCHA -->
+				<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
+				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+				<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+			</form>
+
+			<!--empty area for form error/success output-->
+			<div class="row">
+				<div class="col-xs-12">
+					<div id="output-area"></div>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
