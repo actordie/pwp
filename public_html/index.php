c22d46d14bc50d0c77f6df28eff35ca2f280ea1a
S JACKSON <jackson.sabastian@gmail.com>
2017-06-06 15:11:20 -0600

    added coloumns and img responsivness to thumbnails... aren't adjusting desirably

--- public_html/index.php
+++ public_html/index.php
@@ -10,10 +10,10 @@
 
 		<!-- Bootstrap/ Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css"/>
 
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -27,7 +27,8 @@
 
 		<!-- Bootstrap - JS  Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 		<!-- Fancybox -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
 	</head>
@@ -79,13 +80,13 @@
 
 		<div class="background-image-4" id="contact">
 		</div>
-/*
+		/*
 		<!--
 		<?php
-		foreach ($imgArray as $image) {
+		foreach($imgArray as $image) {
 			echo '<div class="col-xs-12 col-md-12"> 
-				<a data-fancybox="gallery" href='."images/fancybox-fullsize-images/$image".'>
-					<img src='."images/images-thumbnail/$image".' alt="$image">
+				<a data-fancybox="gallery" href=' . "images/fancybox-fullsize-images/$image" . '>
+					<img src=' . "images/images-thumbnail/$image" . ' alt="$image">
 				</a>
 			  </div>';
 		}
@@ -98,73 +99,75 @@
 			<img src="images/pov-resize.jpg">
 		</a>
 -->
-		<div id="gallery">
-			<div class="container-fluid">
-				<a data-fancybox="group"  data-width="800" data-heigt="100" href="images/regina-revenge.jpg" class="thumbnail">
-					<img class="thumbnail" src="images/regina-revenge.jpg">
-				</a>
-
-				<a class="thumbnail" data-fancybox="group" href="images/pov-resize.jpg">
-					<img src="images/pov-resize.jpg" class="thumbnail">
-				</a>
-			</div>
-		</div>
-
-
-		<!--Begin Contact Form-->
-		<div>
-			<form id="contact-form" action="php/mailer.php" method="post">
-				<div class="form-group">
-					<label for="name">Name <span class="text-danger">*</span></label>
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="fa fa-user" aria-hidden="true"></i>
+		<div class="container-fluid">
+			<div class="row thumbnail-row">
+				<div id="gallery">
+					<a data-fancybox="group" href="images/regina-revenge.jpg" class="thumbnail">
+						<div class="col-xs-12 col-md-3">
+							<img class="thumbnail" src="images/regina-revenge.jpg">
+							<a data-fancybox="group" href="images/pov-resize.jpg" class="thumbnail">
+								<div class="col-xs-12 col-md-3">
+									<img class="thumbnail img-responsive" src="images/pov-resize.jpg">
+								</div>
 						</div>
-						<input type="text" class="form-control" id="name" name="name" placeholder="Name">
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="email">Email <span class="text-danger">*</span></label>
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
-						</div>
-						<input type="email" class="form-control" id="email" name="email" placeholder="Email">
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="subject">Subject</label>
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="fa fa-pencil" aria-hidden="true"></i>
-						</div>
-						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="message">Message <span class="text-danger">*</span></label>
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="fa fa-comment" aria-hidden="true"></i>
-						</div>
-						<textarea class="form-control" rows="5" id="message" name="message"
-									 placeholder="Message (2000 characters max)"></textarea>
-					</div>
-				</div>
-
-				<!-- reCAPTCHA -->
-				<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
-
-				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-				<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-			</form>
-
-			<!--empty area for form error/success output-->
-			<div class="row">
-				<div class="col-xs-12">
-					<div id="output-area"></div>
 				</div>
 			</div>
-		</div>
+
+
+			<!--Begin Contact Form-->
+			<div>
+				<form id="contact-form" action="php/mailer.php" method="post">
+					<div class="form-group">
+						<label for="name">Name <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-user" aria-hidden="true"></i>
+							</div>
+							<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="email">Email <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</div>
+							<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="subject">Subject</label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-pencil" aria-hidden="true"></i>
+							</div>
+							<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="message">Message <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-comment" aria-hidden="true"></i>
+							</div>
+							<textarea class="form-control" rows="5" id="message" name="message"
+										 placeholder="Message (2000 characters max)"></textarea>
+						</div>
+					</div>
+
+					<!-- reCAPTCHA -->
+					<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
+					<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+					<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+				</form>
+
+				<!--empty area for form error/success output-->
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="output-area"></div>
+					</div>
+				</div>
+			</div>
 	</body>
 </html>
